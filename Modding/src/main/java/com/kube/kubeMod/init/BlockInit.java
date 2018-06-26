934b87948d337c1697a6c5fc2db68c2e4050bd48
Hardvor7 <34392505+Hardvor7@users.noreply.github.com>
2018-06-26 20:36:00 +0200

    Add ColdIron Item/Block

--- Modding/src/main/java/com/kube/kubeMod/init/BlockInit.java
+++ Modding/src/main/java/com/kube/kubeMod/init/BlockInit.java
@@ -3,10 +3,14 @@ package com.kube.kubeMod.init;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.kube.kubeMod.blocks.ColdIronBlock;
+
 import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 
 public class BlockInit 
 {
 	public static final List<Block> BLOCKS = new ArrayList<Block>();
-	
+
+	public static final Block BLOCK_COLDIRON = new ColdIronBlock("block_coldiron", Material.IRON);
 }
--- Modding/src/main/java/com/kube/kubeMod/init/ItemInit.java
+++ Modding/src/main/java/com/kube/kubeMod/init/ItemInit.java
@@ -3,10 +3,14 @@ package com.kube.kubeMod.init;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.kube.kubeMod.items.ItemBase;
+
 import net.minecraft.item.Item;
 
 public class ItemInit 
 {
 	public static final List<Item> ITEMS = new ArrayList<Item>();
-	
+
+	// Items
+	public static final Item COLDIRON_INGOT = new ItemBase("ingot_coldiron");
 }
