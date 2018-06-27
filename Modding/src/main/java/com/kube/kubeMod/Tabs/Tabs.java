36ec3c472b5272083b691c25c82310fa161dd7fb
benjaminChibrac <40595462+benjaminChibrac@users.noreply.github.com>
2018-06-27 15:03:24 +0200

    CreativeTabs
    Heyyyyyyyyyyyyyyyyyy salut tout le monde c'est DavidLafargePokemon

---
+++ Modding/src/main/java/com/kube/kubeMod/Tabs/Tabs.java
@@ -0,0 +1,22 @@
+package com.kube.kubeMod.Tabs;
+
+import com.kube.kubeMod.init.ItemInit;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
+
+public class Tabs {
+
+	public static final CreativeTabs tabKubeMod = new CreativeTabs("tabTestMod") {
+	
+		@Override
+		public ItemStack getTabIconItem() {
+			return new ItemStack(ItemInit.INGOT_COLDIRON);
+		}
+		
+		@Override
+		public boolean hasSearchBar() {
+			return false;
+		}
+	}.setBackgroundImageName("item_search.png");
+}
